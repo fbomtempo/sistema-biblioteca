a538d8dcdd55243c912c0761e580063224757c6e
Felipe Bomtempo <felipeb.01@outlook.com>
2022-06-23 02:23:35 -0300

    Componente de visualização de devoluções

--- sistema-biblioteca-frontend/src/app/devolucoes/devolucao-detalhes/devolucao-detalhes.component.ts
+++ sistema-biblioteca-frontend/src/app/devolucoes/devolucao-detalhes/devolucao-detalhes.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { Devolucao } from '../models/devolucao';
 
 @Component({
   selector: 'app-devolucao-detalhes',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DevolucaoDetalhesComponent implements OnInit {
 
-  constructor() { }
+  devolucao!: Devolucao;
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.devolucao = this.mascara(this.route.snapshot.data['devolucao']);
   }
 
+  private mascara(devolucao: Devolucao): Devolucao {
+    devolucao.dataDevolucao = new Date(devolucao.dataDevolucao).toLocaleDateString('pt-BR', { timeZone: 'UTC' });
+    devolucao.emprestimo.cliente.cpf =  devolucao.emprestimo.cliente.cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
+    return devolucao;
+  }
+
+  alterar() {
+    this.router.navigate([`/devolucoes/editar/${this.devolucao.id}`]);
+  }
+
+  voltar(): void {
+    this.router.navigate(['/devolucoes']);
+  }
 }
